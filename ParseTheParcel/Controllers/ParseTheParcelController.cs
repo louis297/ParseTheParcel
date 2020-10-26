3186c88a4124f5cf4bf1cafb68ce7307c2ce72bd
xl297 <xl297@georgetown.edu>
2020-10-27 00:23:41 +1300

    add empty controller and service

---
+++ ParseTheParcel/Controllers/ParseTheParcelController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using ParseTheParcel.Services.ParseAndParcelServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParseTheParcel.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ParseTheParcelController : Controller
+    {
+        private IParseAndParcelService _service;
+        public ParseTheParcelController(IParseAndParcelService service)
+        {
+            _service = service;
+        }
+    }
+}
---
+++ ParseTheParcel/Services/ParseAndParcelServices/IParseAndParcelService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParseTheParcel.Services.ParseAndParcelServices
+{
+    public interface IParseAndParcelService
+    {
+    }
+}
