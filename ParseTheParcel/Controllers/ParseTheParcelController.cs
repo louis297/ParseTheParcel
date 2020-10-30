f693b71cbc3a2cff4b4dbc88d8f9e5f47d4245a4
xl297 <xl297@georgetown.edu>
2020-10-31 04:09:56 +1300

    implement controller

--- ParseTheParcel/Controllers/ParseTheParcelController.cs
+++ ParseTheParcel/Controllers/ParseTheParcelController.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using ParseTheParcel.Config;
 using ParseTheParcel.DTOs.ParseTheParcelDTOs;
+using ParseTheParcel.ResponseModels.ParseTheParcelResponseModels;
 using ParseTheParcel.Services.ParseAndParcelServices;
 using System;
 using System.Collections.Generic;
@@ -25,7 +26,7 @@ namespace ParseTheParcel.Controllers
 
         [HttpGet]
         [Route("parsetheparcel")]
-        public void ParseTheParcel([FromQuery]ParcelDTO parcel)
+        public ParseTheParcelResponseModel ParseTheParcel([FromQuery]ParcelDTO parcel)
         {
             try
             {
@@ -33,15 +34,37 @@ namespace ParseTheParcel.Controllers
                 if (price is double priceValue)
                 {
                     _logger.LogInformation(ParseTheParcelEventID.Done, $"The parcel {parcel.Length}x{parcel.Breadth}x{parcel.Height} @ {parcel.Weight / 1000.0} kg costs ${priceValue}");
+                    return new ParseTheParcelResponseModel
+                    {
+                        isSuccess = true,
+                        Message = "",
+                        Price = priceValue
+                    };
                 }
                 else
                 {
                     // parcel is too big or too heavy
                     _logger.LogInformation(ParseTheParcelEventID.TooLarge, $"The parcel {parcel.Length}x{parcel.Breadth}x{parcel.Height} @ {parcel.Weight / 1000.0} kg is too large");
+                    return new ParseTheParcelResponseModel
+                    {
+                        isSuccess = false,
+                        Message = "The parcel is either too large or too heavy",
+                        Price = 0
+                    };
                 }
             } catch(Exception e){
                 _logger.LogInformation(ParseTheParcelEventID.InternalError, e, $"Exception when parsing parcel {parcel.Length}x{parcel.Breadth}x{parcel.Height} @ {parcel.Weight / 1000.0} kg.");
+                throw e;
             }
+        }
+
+        [HttpGet]
+        [Route("reloadrules")]
+        // TODO: add auth control to reload rules function
+        public IActionResult ReloadRules()
+        {
+            _service.ReloadRules();
+            return Ok("Reload rules successfully");
         }
     }
 }
