4c59b251f7c0cadc4ee4a6ac3b4142c1faeda798
xl297 <xl297@georgetown.edu>
2020-10-30 23:55:12 +1300

    add service

---
+++ ParseTheParcel/Services/ParseTheParcelServices/IParseTheParcelService.cs
@@ -0,0 +1,14 @@
+﻿using ParseTheParcel.DTOs.ParseTheParcelDTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParseTheParcel.Services.ParseAndParcelServices
+{
+    public interface IParseTheParcelService
+    {
+        public double? ParseTheParcel(ParcelDTO parcel);
+        public void UpdateRules();
+    }
+}
---
+++ ParseTheParcel/Services/ParseTheParcelServices/ParseTheParcelService.cs
@@ -0,0 +1,38 @@
+﻿using ParseTheParcel.DTOs.ParseTheParcelDTOs;
+using ParseTheParcel.Models;
+using ParseTheParcel.Models.ParseTheParcelModels;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParseTheParcel.Services.ParseAndParcelServices
+{
+    public class ParseTheParcelService: IParseTheParcelService
+    {
+        private ParseTheParcelDbContext _dbContext;
+        List<ParseTheParcelRule> parseRules;
+        public ParseTheParcelService(ParseTheParcelDbContext dbContext)
+        {
+            _dbContext = dbContext;
+            parseRules = new List<ParseTheParcelRule>();
+            UpdateRules();
+        }
+        public double? ParseTheParcel(ParcelDTO parcel)
+        {
+            foreach(var rule in parseRules)
+            {
+
+            }
+            return null;
+        }
+        public void UpdateRules()
+        {
+            parseRules = _dbContext
+                .parseTheParcelRules
+                .OrderBy(p => p.RuleOrder)
+                .ToList();
+        }
+    }
+}
