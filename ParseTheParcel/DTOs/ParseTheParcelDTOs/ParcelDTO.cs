ad6e90cbd33127d05fda8c55f189a6be3adfc1be
xl297 <xl297@georgetown.edu>
2020-10-30 23:56:54 +1300

    add DTO

---
+++ ParseTheParcel/DTOs/ParseTheParcelDTOs/ParcelDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParseTheParcel.DTOs.ParseTheParcelDTOs
+{
+    public class ParcelDTO
+    {
+        public int Length { get; set; } // mm
+        public int Breadth { get; set; } // mm
+        public int Height { get; set; } // mm
+        public int Weight { get; set; } // gram
+    }
+}
