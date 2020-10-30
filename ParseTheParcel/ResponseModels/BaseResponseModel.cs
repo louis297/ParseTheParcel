6d50bd62210de04742c998b2a2cc83a63a08e643
xl297 <xl297@georgetown.edu>
2020-10-31 04:09:34 +1300

    add response model

---
+++ ParseTheParcel/ResponseModels/BaseResponseModel.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace MyRental.Models.ResponseModels
+{
+    public abstract class BaseResponseModel
+    {
+        public bool isSuccess { get; set; }
+        public string Message { get; set; }
+    }
+}
---
+++ ParseTheParcel/ResponseModels/ParseTheParcelResponseModels/ParseTheParcelResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using MyRental.Models.ResponseModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ParseTheParcel.ResponseModels.ParseTheParcelResponseModels
+{
+    public class ParseTheParcelResponseModel: BaseResponseModel
+    {
+        public double Price { get; set; }
+    }
+}
