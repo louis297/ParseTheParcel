61fefa8ff54f081154ddc1851d9c164590272b4d
xl297 <xl297@georgetown.edu>
2020-11-03 08:43:53 +1300

    build test project

---
+++ ParseTheParcelTest/ParseTheParcelControllersTest/ParseTheParcelControllerTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using ParseTheParcel.DTOs.ParseTheParcelDTOs;
+using ParseTheParcel.Services.ParseAndParcelServices;
+using ParseTheParcelTest.ServicesTest;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParseTheParcelTest.ParseTheParcelControllersTest
+{
+    [TestFixture]
+    class ParseTheParcelControllerTest
+    {
+        private IParseTheParcelService service;
+        
+        public ParseTheParcelControllerTest()
+        {
+            service = new ParseTheParcelServiceMock();
+        }
+
+        [Test]
+        public void ParseTheParcel()
+        {
+            Assert.AreEqual(service.ParseTheParcel(new ParcelDTO {Length=100, Breadth=200, Height=100, Weight=1000}), 5.0);
+            Assert.AreEqual(service.ParseTheParcel(new ParcelDTO { Length = 100, Breadth = 700, Height = 100, Weight = 1000 }), null);
+            Assert.AreEqual(service.ParseTheParcel(new ParcelDTO { Length = 100, Breadth = 500, Height = 100, Weight = 1000 }), 8.5);
+            Assert.AreEqual(service.ParseTheParcel(new ParcelDTO { Length = 100, Breadth = 200, Height = 180, Weight = 1000 }), 7.5);
+            Assert.AreEqual(service.ParseTheParcel(new ParcelDTO { Length = 100, Breadth = 200, Height = 222, Weight = 1000 }), 8.5);
+            Assert.AreEqual(service.ParseTheParcel(new ParcelDTO { Length = 100, Breadth = 200, Height = 100, Weight = 100000 }), null);
+        }
+    }
+}
---
+++ ParseTheParcelTest/ServicesTest/ParseTheParcelServiceMock.cs
@@ -0,0 +1,72 @@
+﻿using ParseTheParcel.DTOs.ParseTheParcelDTOs;
+using ParseTheParcel.Models.ParseTheParcelModels;
+using ParseTheParcel.Services.ParseAndParcelServices;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParseTheParcelTest.ServicesTest
+{
+    class ParseTheParcelServiceMock : IParseTheParcelService
+    {
+        List<ParseTheParcelRule> _rules;
+        public ParseTheParcelServiceMock()
+        {
+            _rules = new List<ParseTheParcelRule> {
+                new ParseTheParcelRule
+                {
+                    RuleID = 1,
+                    RuleName = "Small",
+                    RuleOrder = 1,
+                    Length = 200,
+                    Breadth = 300,
+                    Height = 150,
+                    Weight = 25000,
+                    Price = 5.0
+                },
+                new ParseTheParcelRule
+                {
+                    RuleID = 2,
+                    RuleName = "Medium",
+                    RuleOrder = 2,
+                    Length = 300,
+                    Breadth = 400,
+                    Height = 200,
+                    Weight = 25000,
+                    Price = 7.5
+                },
+                new ParseTheParcelRule
+                {
+                    RuleID = 3,
+                    RuleName = "Large",
+                    RuleOrder = 3,
+                    Length = 400,
+                    Breadth = 600,
+                    Height = 250,
+                    Weight = 25000,
+                    Price = 8.5
+                }
+            };
+        }
+        public double? ParseTheParcel(ParcelDTO parcel)
+        {
+            foreach (var rule in _rules)
+            {
+                if (parcel.Length <= rule.Length
+                    && parcel.Breadth <= rule.Breadth
+                    && parcel.Height <= rule.Height
+                    && parcel.Weight <= rule.Weight
+                  )
+                {
+                    return rule.Price;
+                }
+            }
+            return null;
+        }
+
+        public void ReloadRules()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
